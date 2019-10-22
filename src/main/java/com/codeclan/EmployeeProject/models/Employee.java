4f995af91fec3ec9f5b250dec575f1179d92a5be
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-22 11:10:00 +0100

    set up employee attributes

---
+++ src/main/java/com/codeclan/EmployeeProject/models/Employee.java
@@ -0,0 +1,23 @@
+package com.codeclan.EmployeeProject.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "employee_number")
+    private int employeeNumber;
+
+
+}
