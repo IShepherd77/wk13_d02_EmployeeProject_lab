403ea6f8d7dd96d04a2c71c1bae5a48564ce731d
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-22 11:23:05 +0100

    created project class

---
+++ src/main/java/com/codeclan/EmployeeProject/models/Project.java
@@ -0,0 +1,50 @@
+package com.codeclan.EmployeeProject.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "duration")
+    private int duration;
+
+    public Project(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
