56a30461b892fbaba5507084dec08364157644b3
Ian Shepherd <ianshepherd77@gmail.com>
2019-10-22 11:14:34 +0100

    added getters and setters to employee

--- src/main/java/com/codeclan/EmployeeProject/models/Employee.java
+++ src/main/java/com/codeclan/EmployeeProject/models/Employee.java
@@ -20,4 +20,45 @@ public class Employee {
     private int employeeNumber;
 
 
+
+    public Employee(String firstName, String lastName, int employeeNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.employeeNumber = employeeNumber;
+    }
+
+    public Employee(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
 }
